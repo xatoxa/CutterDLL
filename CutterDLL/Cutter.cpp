bffd671441c255c40c676eccc830f4584c8429f3
Anton Hatuncev <anton.xatuncev@mail.ru>
2019-01-30 15:16:29 +0300

    скелет Cut + changed project settings in Linker

--- CutterDLL/Cutter.cpp
+++ CutterDLL/Cutter.cpp
@@ -8,11 +8,11 @@
 
 
 
-STDMETHODIMP CCutter::Cut_image(HBITMAP* image, unsigned int* x1, unsigned int* y1, unsigned int* x2, unsigned int* y2, HBITMAP* out_param)
+STDMETHODIMP CCutter::Cut_image(HBITMAP* image, unsigned int* x1, unsigned int* y1, unsigned int* x2, unsigned int* y2, HBITMAP* out_image)
 {
 	// TODO: Добавьте сюда код реализации.
 
-	
+	*out_image = *image;
 
 	return S_OK;
 }
